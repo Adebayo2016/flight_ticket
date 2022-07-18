a4cf69e257a10bbf2849cee01e4c060c38add184
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-07-18 15:46:28 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/flight/my_flight/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flight.my_flight
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/bottom_nav.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
