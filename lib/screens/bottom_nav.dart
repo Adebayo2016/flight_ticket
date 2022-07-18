d650d7923c4079ee24afb54879be6db7e7973007
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-07-18 16:00:52 +0100

    Updated commit

--- lib/screens/bottom_nav.dart
+++ lib/screens/bottom_nav.dart
@@ -10,6 +10,11 @@ class _BottomBarState extends State<BottomBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title:  const Text('flight ticket'),
+
+
+      ),
 
     );
   }
