5cb6661e77b7db6997a182eca3538e04d314f1df
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-07-19 21:16:37 +0100

    Added more Stuffs

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:my_flight/utils/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,33 +16,53 @@ class HomeScreen extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               children: [
+                Gap(40),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Good Morning", style: Styles.headlineStyle3,),
-                        Text('Book Tickets', style: Styles.headlineStyle,),
+                        Text(
+                          "Good Morning",
+                          style: Styles.headlineStyle3,
+                        ),
+                        Gap(5),
+                        Text(
+                          'Book Tickets',
+                          style: Styles.headlineStyle,
+                        ),
                       ],
                     ),
-
                     Container(
                       height: 50,
                       width: 50,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
+                          borderRadius: BorderRadius.circular(10.0),
                           image: const DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage('assets/images/img_1.png'),
-                      )),
+                            fit: BoxFit.cover,
+                            image: AssetImage('assets/images/img_1.png'),
+                          )),
                     )
+                  ],
+                ),
+                Gap(25),
+                Row(
+                  children: [
+                    Gap(25),
+                    Icon(
+                      FluentSystemIcons.ic_fluent_search_regular,
+                      color: Color(0xffbfc205),
+                    ),
+                    Text(
+                      'Search',
+                      style: Styles.headlineStyle4,
+                    ),
                   ],
                 )
               ],
             ),
-          )
+          ),
         ],
       ),
     );
