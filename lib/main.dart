5c5d55c107f184225dba74fb039b108bd8b5c037
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-07-19 17:01:43 +0100

    Added more Stuffs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_flight/screens/bottom_nav.dart';
+import 'package:my_flight/utils/app_styles.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: primary,
       ),
       home: const BottomBar( ),
     );
