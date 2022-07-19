8b1ccaea9c83560cf038e485b1f69cb0ccf44f61
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-07-19 16:54:38 +0100

    Added more Stuffs

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+Color primary = const Color(0xFF687daf);
+
+class Styles {
+  static Color  primaryColor= primary;
+  static Color textColor = Color(0xff3b3b3b);
+  static Color  bgColor = Color(0xffeeedf3);
+  static Color orangeColor = Color(0xFF526799);
+  static TextStyle textStyle= TextStyle(fontSize: 16,color: textColor, fontWeight: FontWeight.w500);
+  static TextStyle headline= TextStyle(fontSize: 16,color: textColor, fontWeight: FontWeight.w500);
+
+
+}
