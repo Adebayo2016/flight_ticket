5cb39ee76cbc209a2bd99cadbc0ff5c72d3f283c
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-07-20 13:16:54 +0100

    Added more Stuffs

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -33,6 +33,7 @@ class HomeScreen extends StatelessWidget {
                           style: Styles.headlineStyle,
                         ),
                       ],
+
                     ),
                     Container(
                       height: 50,
