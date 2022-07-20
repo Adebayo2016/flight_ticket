fad390f7a3009bae2b6dbf7d03704749ba3ee058
Adebayo2016 <adewaleadebayo630@gmail.com>
2022-07-20 16:10:16 +0100

    Added more Stuffs

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:my_flight/utils/app_layout.dart';
 class TicketView extends StatelessWidget {
   const TicketView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    final size = AppLayout.getSize(context);
+
+    return SizedBox(
+      width: size.width,
+      height: 200,
+      child: Container(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(16),
+            )
+          ],
+        ),
+      ),
+
+
+    );
   }
 }
